313b06d21998aa061a9a1ef8c4e8b910c03cee02
iBarzha <grytcaie@gmail.com>
2024-10-09 12:19:06 +0300

    4-8 part (end tutorial)

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,19 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
+
+
+class Question(models.Model):
+    # ...
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -18,3 +31,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
